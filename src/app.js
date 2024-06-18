7060b7cceff416db89d5542205df4c44b9848cbc
Andrea Nanna <andrea.nanna@wink.codes>
2024-06-18 15:46:41 +0200

    total rewrite lol

--- src/app.js
+++ src/app.js
@@ -4,50 +4,56 @@ import { calculate } from './calculate.js'
 const app = express()
 app.set('view engine', 'pug')
 app.use(express.json())
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-)
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', function (req, res) {
-    let results = req.query.results
-
-    res.render('index', {
-        results: results || '',
-    })
+app.get('/', (req, res) => {
+    const results = req.query.results || ''
+    res.render('index', { results })
 })
 
-app.post('/calculate', function (req, res) {
-    let resultValue
+app.post('/calculate', (req, res) => {
+    const {
+        timeSent,
+        peopleCount: peopleCountStr,
+        kebabCount: kebabCountStr,
+        roundPizzaCount: roundPizzaCountStr,
+        saladCount: saladCountStr,
+        slicedPizzaCount: slicedPizzaCountStr,
+    } = req.body
 
-    let params = {
-        timeSent: req.body.timeSent,
-        peopleCount: parseInt(req.body.peopleCount),
-        preparation: {weight: 1, variance: 5},
+    const peopleCount = parseInt(peopleCountStr)
+    const kebabCount = parseInt(kebabCountStr)
+    const roundPizzaCount = parseInt(roundPizzaCountStr)
+    const saladCount = parseInt(saladCountStr)
+    const slicedPizzaCount = parseInt(slicedPizzaCountStr)
+
+    const params = {
+        timeSent,
+        peopleCount,
+        preparation: { weight: 1, variance: 5 },
         orderData: {
-            kebab: { count: parseInt(req.body.kebabCount), weight: 5, variance: 2 },
-            roundPizza: { count: parseInt(req.body.roundPizzaCount), weight: 6, variance: 3 },
-            salad: { count: parseInt(req.body.saladCount), weight: 2, variance: 3 },
-            slicedPizza: { count: parseInt(req.body.slicedPizzaCount), weight: 1, variance: 2 },
+            kebab: { count: kebabCount, weight: 5, variance: 2 },
+            roundPizza: { count: roundPizzaCount, weight: 6, variance: 3 },
+            salad: { count: saladCount, weight: 2, variance: 3 },
+            slicedPizza: { count: slicedPizzaCount, weight: 1, variance: 2 },
             other: {
-                count: 0,
+                count: peopleCount - (kebabCount + roundPizzaCount + saladCount + slicedPizzaCount),
                 weight: 3,
                 variance: 5,
-                },
-            drink: { count: parseInt(req.body.peopleCount), weight: 0, variance: 1 },
+            },
+            drink: { count: peopleCount, weight: 0, variance: 1 },
         },
-        travelTime: { weight: 15, variance: 10},
-        walkTime: {weight: 4, variance: 2},
-        multipliers: {fast: 0.85, slow: 1.2}
+        travelTime: { weight: 15, variance: 10 },
+        walkTime: { weight: 4, variance: 2 },
+        multipliers: { fast: 0.85, slow: 1.2 },
     }
 
-    params.orderData.other.count = params.peopleCount - (params.orderData.kebab.count + params.orderData.roundPizza.count + params.orderData.salad.count)
-
-    resultValue = calculate(params)
-
-    res.redirect('/?results=' + resultValue)
+    const resultValue = calculate(params)
+    res.redirect(`/?results=${resultValue}`)
 })
 
 const port = process.env.PORT || 3001
-app.listen(port)
+app.listen(port, () => {
+    console.log('--- App is running at http://localhost:%d', port)
+    console.log('  Press CTRL-C to stop\n')
+})
--- src/calculate.js
+++ src/calculate.js
@@ -1,106 +1,80 @@
 export function calculate(params) {
-    let minutesToAdd = 0
-    let finalTime
-    let random = 0
-    let additionalMinutes = 0
-    console.log(params)
-
+    console.log(params);
     if (params.orderData.other.count < 0) {
-        console.log('Wrong order count')
-        return 'Numero di cibo superiore al numero di persone, stai a trollà'
+        console.log('Wrong order count');
+        return 'Numero di cibo superiore al numero di persone, stai a trollà';
     }
 
-    //Start preparing order
-    random = getRandomInt(0, params.preparation.variance)
-    additionalMinutes = params.preparation.weight + random
-    minutesToAdd += additionalMinutes
-    console.log(
-        `Order received! Sending it to the kitchen: adding ${additionalMinutes} (${params.preparation.weight} + ${random}) minutes\nTotal minutes: ${minutesToAdd}`
-    )
+    let minutesToAdd = 0;
 
-    //Prepare food and drinks
-    for (var foodName in params.orderData) {
-        var food = params.orderData[foodName]
+    const calculateAdditionalMinutes = (weight, variance) => {
+        const random = getRandomInt(0, variance);
+        return {value: weight + random, random: random};
+    };
+
+    const logAndAddMinutes = (context, weight, variance) => {
+        const minutesCalculation = calculateAdditionalMinutes(weight, variance);
+        minutesToAdd += minutesCalculation.value;
+        console.log(`${context}: adding ${minutesCalculation.value} (${weight} + ${minutesCalculation.random}) minutes\nTotal minutes: ${minutesToAdd}`);
+    };
+
+    // Start preparing order
+    logAndAddMinutes('Order received! Sending it to the kitchen', params.preparation.weight, params.preparation.variance);
+
+    // Prepare food and drinks
+    for (let foodName in params.orderData) {
+        let food = params.orderData[foodName];
         for (let i = 0; i < food.count; i++) {
-            random = getRandomInt(0, food.variance)
-            additionalMinutes = food.weight + random
-            minutesToAdd += additionalMinutes
-            console.log(`Preparing one ${foodName}, adding ${additionalMinutes} (${food.weight} + ${random}) minutes\nTotal minutes: ${minutesToAdd}`)
+            logAndAddMinutes(`Preparing one ${foodName}`, food.weight, food.variance);
         }
     }
 
-    //Get order ready
-    random = getRandomInt(0, params.preparation.variance)
-    additionalMinutes = params.preparation.weight + random
-    minutesToAdd += additionalMinutes
-    console.log(
-        `Order done! Packing your food up: adding ${additionalMinutes} (${params.preparation.weight} + ${random}) minutes\nTotal minutes: ${minutesToAdd}`
-    )
+    // Get order ready
+    logAndAddMinutes('Order done! Packing your food up', params.preparation.weight, params.preparation.variance);
 
-    //Car time
-    random = getRandomInt(0, params.travelTime.variance)
-    additionalMinutes = params.travelTime.weight + random
-    minutesToAdd += additionalMinutes
-    console.log(
-        `Moving from Casal Bertone to WiNK office by car: adding ${additionalMinutes} (${params.travelTime.weight} + ${random}) minutes\nTotal minutes: ${minutesToAdd}`
-    )
+    // Car time
+    logAndAddMinutes('Moving from Casal Bertone to WiNK office by car', params.travelTime.weight, params.travelTime.variance);
 
-    //Walking to office
-    random = getRandomInt(0, params.walkTime.variance)
-    additionalMinutes = params.walkTime.weight + random
-    minutesToAdd += additionalMinutes
-    console.log(
-        `Almost here! Walking to your office: adding ${additionalMinutes} (${params.walkTime.weight} + ${random}) minutes\nTotal minutes: ${minutesToAdd}`
-    )
+    // Walking to office
+    logAndAddMinutes('Almost here! Walking to your office', params.walkTime.weight, params.walkTime.variance);
 
-    let speed = getRandomInt(0, 2)
-    console.log('Speed multiplier: ' + (speed === 0 ? 'none' : speed === 1 ? 'faster' : 'slower') + ' (' + speed + ')')
-
-    if (speed === 1) {
-        let newTime = Math.floor(minutesToAdd * params.multipliers.fast)
-        console.log(`Fast = ${minutesToAdd}` + ' * ' + `${params.multipliers.fast} =`, newTime)
-        minutesToAdd = newTime
-    } else if (speed === 2) {
-        let newTime = Math.floor(minutesToAdd * params.multipliers.slow)
-        console.log(`Slow = ${minutesToAdd}` + ' * ' + `${params.multipliers.slow} =`, newTime)
-        minutesToAdd = newTime
+    // Speed multiplier
+    const speed = getRandomInt(0, 2);
+    console.log('Speed multiplier: ' + (speed === 0 ? 'none' : speed === 1 ? 'faster' : 'slower') + ' (' + speed + ')');
+    if (speed !== 0) {
+        const multiplier = speed === 1 ? params.multipliers.fast : params.multipliers.slow;
+        const newTime = Math.floor(minutesToAdd * multiplier);
+        console.log(`${speed === 1 ? 'Fast' : 'Slow'} = ${minutesToAdd} * ${multiplier} = ${newTime}`);
+        minutesToAdd = newTime;
     }
 
-    console.log(`Order simulation complete! Final minutes calculated = ${minutesToAdd}`)
-    finalTime = calculateTime(params.timeSent, minutesToAdd)
-    return finalTime
+    console.log(`Order simulation complete! Final minutes calculated = ${minutesToAdd}`);
+    const finalTime = calculateTime(params.timeSent, minutesToAdd);
+    return finalTime;
 }
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function calculateTime(timeSent, minutesToAdd) {
-    const [hours, minutes] = timeSent.split(':').map(Number)
-    console.log('Time the order was sent (h, m) =', hours, minutes)
+    const [hours, minutes] = timeSent.split(':').map(Number);
+    console.log('Time the order was sent (h, m) =', hours, minutes);
     if (minutes < 0 || minutes > 59 || hours < 0 || hours > 23 || minutes == undefined || hours == undefined) {
-        return 'Orario fatto male uddio'
+        return 'Orario fatto male uddio';
     }
-    const currentDate = new Date()
-    currentDate.setHours(hours, minutes, 0, 0)
-    let timestamp = Math.floor(currentDate.getTime() / 1000)
 
-    console.log(`Adding ${minutesToAdd} minutes to when the order was sent...`)
-    timestamp += minutesToAdd * 60
+    const currentDate = new Date();
+    currentDate.setHours(hours, minutes, 0, 0);
+    let timestamp = Math.floor(currentDate.getTime() / 1000);
 
-    const date = new Date(timestamp * 1000)
-    const dateOptions = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    }
-    const timeOptions = {
-        hour: '2-digit',
-        minute: '2-digit',
-    }
-    const formattedDate = date.toLocaleDateString('it-IT', dateOptions)
-    const formattedTime = date.toLocaleTimeString('it-it', timeOptions)
+    console.log(`Adding ${minutesToAdd} minutes to when the order was sent...`);
+    timestamp += minutesToAdd * 60;
 
-    console.log(`... your food will arrive at ${formattedDate}, ${formattedTime}`)
-    return `${formattedDate}, ${formattedTime}`
+    const date = new Date(timestamp * 1000);
+    const formattedDate = date.toLocaleDateString('it-IT', { year: 'numeric', month: 'long', day: 'numeric' });
+    const formattedTime = date.toLocaleTimeString('it-IT', { hour: '2-digit', minute: '2-digit' });
+
+    console.log(`... your food will arrive at ${formattedDate}, ${formattedTime}`);
+    return `${formattedDate}, ${formattedTime}`;
 }
